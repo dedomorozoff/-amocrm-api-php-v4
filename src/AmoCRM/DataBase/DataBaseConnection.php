065545f20f3aabcee9b5d34edf79624de9de6884
Alina Abdul <alinka140901@gmail.com>
2023-07-26 15:09:29 +0300

    ДОбавлен класс сохраняющий токены в базу данных

---
+++ src/AmoCRM/DataBase/DataBaseConnection.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace AmoCRM\DataBase;
+
+/**
+ * Класс DataBaseConnection создает соединение с базой данных
+ * 
+ * Для корректного соединения требуется передать в конструктор класса массив с конфигурационными данными базы данных с ключами:
+ * host - хост на котором расположена БД
+ * port = порт на котором расположена БД
+ * db - название БД
+ * user - имя пользователя
+ * password - пароль пользователя
+ */
+
+class DataBaseConnection {
+
+    private $connection;
+
+    public function __construct(array $config)
+    {
+        $host = $config['host'];
+        $port = $config['port'];
+        $dataBase = $config['db'];
+        $user = $config['user'];
+        $password = $config['password'];
+        $dsn = "mysql:host={$host};port={$port};dbname={$dataBase};";
+        try {
+            $this->connection = new \PDO($dsn, $user, $password);
+        } catch(\PDOException $e) {
+            printf('Не удалось подключиться к базе данных');
+        }
+    }
+
+
+    /**
+     * сохраняет токены в базу данных
+     *
+     * @param string $domain
+     * @param string $tokens
+     * @param string $integrationCode
+     * @return boolean
+     */
+    public function addTokens(string $domain, string $tokens, string $integrationCode) : bool
+    {
+        $sql = "INSERT INTO tokens (token_domain, token_json, token_integration_code)
+                VALUES (:token_domain, :token_json, :token_integration_code)";
+        $query = $this->connection->prepare($sql);
+        return $query->execute([
+            'token_domain' => $domain,
+            'token_json' => $tokens,
+            'token_integration_code' => $integrationCode
+        ]);
+    }  
+
+
+    /**
+     * извлекает токены из базы данных
+     *
+     * @param string $domain
+     * @param string $integrationCode
+     * @return string|false
+     */
+    public function getTokens(string $domain, string $integrationCode) : string|false
+    {
+        $sql = "SELECT * FROM tokens
+                WHERE token_domain = :token_domain
+                AND token_integration_code = :token_integration_code";
+        $query = $this->connection->prepare($sql);
+        $query->execute([
+            'token_domain' => $domain,
+            'token_integration_code' => $integrationCode
+        ]);
+        $data = $query->fetchAll();
+        if (! empty($data)) {  
+            return $data[array_key_first($data)]['token_json'];
+        }
+        return false;
+    }
+}
---
+++ src/AmoCRM/TokenStorage/DatabaseStorage.php
@@ -0,0 +1,72 @@
+<?php
+namespace AmoCRM\TokenStorage;
+
+use AmoCRM\DataBase\DataBaseConnection;
+
+class DatabaseStorage implements TokenStorageInterface
+{
+    protected $integrationCode;
+    private $DBconnection;
+
+    public function __construct(array $dataBaseConfig, string $integrationCode = 'private') 
+    {
+        $this->integrationCode = $integrationCode;
+        $this->DBconnection = new DataBaseConnection($dataBaseConfig);
+    }
+
+    /**
+     * Сохраняет токены
+     * @param  array  $tokens Токены для сохранения
+     * @param  string $domain Полный домен amoCRM
+     * @return void
+     */
+    public function save(array $tokens, string $domain)
+    {
+        $jsonTokens = json_encode($tokens, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
+        if ($jsonTokens === false) {
+            $errorMessage = json_last_error_msg();
+            throw new TokenStorageException("Ошибка JSON-кодирования токенов ({$errorMessage}): " . print_r($tokens, true));
+        }
+
+        if (! $this->DBconnection->addTokens($domain, $jsonTokens, $this->integrationCode)) {
+            throw new TokenStorageException("Не удалось записать в базу данных");
+        }
+    }
+
+    /**
+     * Загружает токены
+     * @param  string $domain Полный домен amoCRM
+     * @return array|null
+     */
+    public function load(string $domain)
+    {
+        $tokensJSON = $this->DBconnection->getTokens($domain, $this->integrationCode);
+        if ($tokensJSON === false) {
+            throw new TokenStorageException("Не удалось извлечь токены из базы данных");
+        }
+        $tokens = json_decode($tokensJSON, true);
+        if (is_null($tokens)) {
+            $errorMessage = json_last_error_msg();
+            throw new TokenStorageException(
+                "Ошибка JSON-декодирования содержимого файла токенов ({$errorMessage})"
+            );
+        }
+
+        return $tokens;
+    }
+
+    /**
+     * Проверяет: существуют ли токены для заданного домена amoCRM,
+     * то есть была ли выполнена первичная авторизация
+     * @param  string  $domain Полный домен amoCRM
+     * @return boolean
+     */
+    public function hasTokens(string $domain) :bool
+    {
+        $tokensJSON = $this->DBconnection->getTokens($domain, $this->integrationCode);
+        if ($tokensJSON === false) {
+            return false;
+        }
+        return true;
+    }
+}
