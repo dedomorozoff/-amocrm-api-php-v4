a22efc488d405bd7489a713b967c17590901e513
Loparev Alexander <loparev.alexander@gmail.com>
2023-11-24 17:01:25 +0700

    Добавлен класс для работы с примечаниями

---
+++ src/AmoCRM/AmoNote.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Класс AmoNote. Содержит методы для работы с примечаниями.
+ *
+ * @author    andrey-tech
+ * @copyright 2020 andrey-tech
+ * @see https://github.com/andrey-tech/amocrm-api-php
+ * @license   MIT
+ *
+ * @version 1.1.0
+ *
+ * v1.0.0 (24.04.2019) Первоначальная версия
+ * v1.1.0 (19.05.2020) Добавлена поддержка параметра $subdomain в конструктор
+ *
+ */
+
+declare(strict_types = 1);
+
+namespace AmoCRM;
+
+class AmoNote extends AmoObject
+{
+    /**
+     * Путь для запроса к API
+     * @var string
+     */
+    const URL = '/api/v4/notes';
+
+      /**
+     * @var bool
+     */
+    public $is_editable;
+
+    /**
+     * @var int
+     */
+    public $entity_id;
+
+    /**
+     * @var string
+     */
+    public $text;
+
+    /**
+     * @var string
+     */
+    public $note_type;
+
+    /**
+     * Конструктор
+     * @param array $data Параметры модели
+     * @param string $subdomain Поддомен amoCRM
+     */
+    public function __construct(array $data = [], $subdomain = null)
+    {
+        parent::__construct($data, $subdomain);
+    }
+
+    /**
+     * Приводит модель к формату для передачи в API
+     * @return array
+     */
+    public function getParams() :array
+    {
+        $params = [];
+
+        $properties = [ 'is_editable', 'entity_id', 'text' ];
+        foreach ($properties as $property) {
+            if (isset($this->$property)) {
+                $params[$property] = $this->$property;
+            }
+        }
+
+        if ($this->note_type == 'common'||
+            $this->note_type == 'sms_in' ||
+            $this->note_type == 'sms_out'
+        ) {
+            $params['params'] = [ 'text' => $this->text ];
+        }
+
+        return array_merge(parent::getParams(), $params);
+    }
+}
